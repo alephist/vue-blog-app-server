546c6e8db82b92271b2f0dab21710568a6686a2f
Justin Necesito <justin.necesito@gmail.com>
2019-05-21 12:52:08 +0800

    For deployment

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
+const compression = require('compression');
 const posts = require('./post');
 const comments = require('./comment');
 const users = require('./user');
@@ -8,6 +10,11 @@ const auth = require('./auth');
 module.exports = function(app) {
   app.use(cors());
   app.use(express.json());
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(helmet());
+    app.use(compression());
+  }
 
   app.use('/api/posts', posts);
   app.use('/api/posts/:id/comments', comments);
